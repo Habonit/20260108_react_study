c4b9240850036a1b001d908dcf2e61f517b7cffe
Habonit <khk172216@gmail.com>
2026-01-22 19:41:34 +0900

    [feat](20260120): Vite 기반 React 투두 앱 초기 구성 추가
    - React 19 + Vite 프로젝트 구조와 ESLint 설정을 새로 추가했습니다.
    - useReducer를 활용한 투두 생성/수정/삭제 기본 로직과 관련 컴포넌트를 구성했습니다.

---
+++ section10/src/components/TodoItem.jsx
@@ -0,0 +1,34 @@
+import './TodoItem.css';
+import { memo } from "react"
+
+const TodoItem = ({id, isDone, content, date, onUpdate, onDelete}) => {
+
+    const onClickDelete = () => {
+        onDelete(id);
+    }
+    const onChangeCheckbox = () => {
+        onUpdate(id);
+    }
+    
+    return (
+        <div className="TodoItem">
+            <input readOnly type="checkbox" checked={isDone} onChange={onChangeCheckbox} />
+            <div className="content"> {content}</div>
+            <div className="date"> {new Date(date).toLocaleDateString()} </div>
+            <button onClick={onClickDelete}> 삭제 </button>
+        </div>
+    )
+};
+
+// const memoizedTodoItem = memo(TodoItem, (prevProps, nextProps) => {
+
+//     if (prevProps.id !== nextProps.id) return false;
+//     if (prevProps.isDone !== nextProps.isDone) return false;
+//     if (prevProps.content !== nextProps.content) return false;
+//     if (prevProps.date !== nextProps.date) return false;
+//     return true;
+// });
+
+// export default memoizedTodoItem;    
+
+export default memo(TodoItem);    
